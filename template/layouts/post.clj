c8f2e7d8421d943c0eac43f4769bea48cf087303
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-12-27 15:21:34 +0100

    Add comments

--- template/layouts/post.clj
+++ template/layouts/post.clj
@@ -11,3 +11,11 @@
 
  ; contents
  [:div {:class "post"} contents]]
+
+[:br]
+
+[:div {:class "comment"}
+ [:p
+  "Do you have a comment? Feel free to send me an "
+  [:a {:href "mailto:nebojsa.stricevic@gmail.com"} "email"]
+  " or message me at " (link "@nstricevic" "http://twitter.com/nstricevic") "."]]
