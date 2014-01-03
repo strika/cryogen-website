07c58e9ffe4c7f824a6bbc9d35a8172de494eeae
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-01-03 18:45:06 +0100

    Update styles

--- _config.clj
+++ _config.clj
@@ -26,9 +26,7 @@
         :charset    "utf-8"
         :site-title "There are ants on the page"
         :twitter    "nstricevic"
-        :css        ["http://fonts.googleapis.com/css?family=Kotta+One"
-                     "http://fonts.googleapis.com/css?family=Imprima"
-                     "/css/foundation.min.css"
+        :css        ["/css/foundation.min.css"
                      "/css/style.css"
                      "/css/desert.css"]
         :device-css []
