05e77376c8dc11cc00aa031d29c3ad5711c9eace
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-01-10 19:33:29 +0100

    PhD article

---
+++ template/posts/2014-01-14-why-i-left-phd.html.clj
@@ -0,0 +1,6 @@
+; @layout post
+; @title Why I left PhD studies
+
+(p "When I was on the last year of master studies, I got recruited from 
+   the university staff to work as a teaching assistant and to start
+   PhD when the masters are over.")
