c02d3a86894b791860120249d302c1bf7941486e
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-09-05 11:43:26 +0200

    Update about

--- template/index.html.clj
+++ template/index.html.clj
@@ -13,11 +13,7 @@
 
   [:p "I live in Novi Sad, Serbia, with my wonderful wife Tanja and son Mihajlo."] 
 
-  [:p "I was lucky enough to own Commodore 64 when I was a kid. And even more that it came
-      with a book called \"What Can Commodore 64 Do?\", that I read so many times. Since then
-      my love for the world of computers never ended."]
-
-  [:p "These days I'm working as a web engineer at " (link "Rendered Text" "http://renderedtext.com") ", Novi Sad."]
+  [:p "I'm a web engineer at " (link "Rendered Text" "http://renderedtext.com") ", Novi Sad."]
 
   [:p "In free time, I enjoy coffee and black tea, dark chocolate, dark beer and " (link "pelinkovac" "https://en.wikipedia.org/wiki/Pelinkovac") ". "
       "I'm also interested in mechanical watches and older cars."]
